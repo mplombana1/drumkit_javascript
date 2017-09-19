a4e4ce4a428cab2c85dd56058eea4c078ad5ca5c
Matthew Lombana <mplombana@gmail.com>
2017-09-19 14:52:35 -0400

    finished javascript logic

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-window.addEventListener('keydown', function(e) {
+function playSound(e) {
   const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
-  if(!audio) return; //stop the function from running all together
-
+  const key = document.querySelector(`div[data-key="${e.keyCode}"]`);
+  if (!audio) return; // stop the function from running all together
+  audio.currentTime = 0; //rewind to the start
   audio.play();
-});
+  key.classList.add('playing');
+}
+
+function removeTransition(e) {
+  if (e.propertyName !== 'transform') return;
+  e.target.classList.remove('playing');
+}
+
+
+
+const keys = Array.from(document.querySelectorAll('.key'));
+keys.forEach(key => key.addEventListener('transitionend', removeTransition));
+window.addEventListener('keydown', playSound);
